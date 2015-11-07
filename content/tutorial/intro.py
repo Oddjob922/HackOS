425434d00eae2133d438ddc5a1ad5cffebe4a3ad
Oddjob922 <tyler.922@hotmail.com>
2015-11-06 21:14:14 -0500

    second commit
    --

--- content/tutorial/intro.py
+++ content/tutorial/intro.py
@@ -1,7 +1,7 @@
 import sys, time, random, os
 from ..effects.print_txt import print_txt
 def tutorialP1():
-	print_txt('../tutorial/narrative.txt')
+	print_txt('..tutorial/narrative.txt')
 	
 def tutorialP2():
-	print "hey"
+	print "not designed yet"
